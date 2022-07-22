aa98fef670bb8c8c45e72ed6dcd2965b8af26826
MTzu <mrm0tze007@gmail.com>
2022-07-22 23:22:40 +0200

    minor improvements to the code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import anki.collection as collection
 import anki
 import json
 import re
+import time
 from kana import kanas
 
 headers = {'accept': 'application/json'}
@@ -13,23 +14,30 @@ TagDict = {
         'Ichidan verb' : 'verb::ichidan',
         'Na-adjective' : 'adjective::na',
         'I-adjective' : 'adjective::i',
-        'Adverb (fukushi)' : 'adverb::fukushi',
+        'fukushi' : 'adverb::fukushi',
         "Adverb taking the 'to' particle" : 'adverb::to',
         'Expressions' : 'expression',
         "Noun which may take the genitive case particle 'no'" : 'noun::no',
-        'Noun, used as a suffix' : 'noun:suffix',
+        'Noun, used as a suffix' : 'noun::suffix',
         'Prefix' : 'prefix',
         'Suffix' : 'suffix'
         }
 
+JishoCount = 0
+
 def JishoLookup(Kanji):
     JishoResponse = []
+    global JishoCount
     for n in range(5):
         # for now this only scrapes jisho-responses
         # which are marked with jlpt-n5 to jlpt-n1 levels
         url = 'https://jisho.org/api/v1/search/words?keyword=*' + Kanji + '*%20%23jlpt-n' + str(5-n)
         response = requests.get(url, headers = headers)
         JishoResponse += json.loads(response.text)['data']
+        JishoCount += 1
+        if JishoCount%40 == 0:
+            print('sleep :3')
+            time.sleep(5)
 
     return JishoResponse
 
@@ -53,7 +61,6 @@ def CreateFurigana(JishoEntry):
         partlen = -(-readinglen//parts)
         furigana = [reading[n*partlen:min([readinglen, n*partlen + partlen])] for n in range(parts)]
         furigana = ''.join([''.join([x, '[',  y, ']']) for x, y in zip(list(word), furigana)])
-        compoundfurigana = None
 
     elif len(idx) == max(idx)+1:
         # This does the same as above,
@@ -64,7 +71,6 @@ def CreateFurigana(JishoEntry):
         partlen = -(-readinglen//parts)
         furiganas = [reading[n*partlen:min([readinglen, n*partlen + partlen])] for n in range(parts)]
         furigana = ''.join([''.join([x, '[', y, ']']) for x, y in zip(list(word[:idx]), furiganas)] + [word[idx:]])
-        compoundfurigana = None
 
     else:
         # This is a truly borked way of adding furigana to words.
@@ -86,12 +92,10 @@ def CreateFurigana(JishoEntry):
                 tmpfurigana[0] = tmpfurigana[0][1:]
 
             furigana = ''.join(tmpfurigana)
-            compoundfurigana = word + '[' + reading + ']'
         except:
             furigana = word + '[' + reading + ']'
-            compoundfurigana = None
 
-    return furigana, compoundfurigana
+    return furigana
 
 def CreateCard(JishoEntry):
     # This will take an entry of Jishos API response and turn it into
@@ -105,7 +109,7 @@ def CreateCard(JishoEntry):
     Tags.append('jlpt::n' + str(max(jlpt)))
     for partofspeech in JishoEntry['senses'][0]['parts_of_speech']:
         for tag in list(TagDict.keys()):
-            if re.match(tag, partofspeech):
+            if re.search(tag, partofspeech):
                 Tags.append(TagDict[tag])
 
     Tags += ['漢字::' + Char for Char in JishoEntry['japanese'][0]['word'] if Char not in kanas]
@@ -120,31 +124,21 @@ def CreateCard(JishoEntry):
         prefix = ' ~'
 
     for tag in JishoEntry['senses'][0]['tags']:
-        if re.match(tag, 'Usually written using kana alone'):
-            Card = '(Usually written using kana alone) ' + '\t'.join([
+        if re.search(tag, 'Usually written using kana alone'):
+            Card = '(usually written using kana alone) ' + '\t'.join([
                 suffix + JishoEntry['japanese'][0]['word'] + prefix,
                 suffix + JishoEntry['japanese'][0]['reading'] + prefix,
                 ', '.join(JishoEntry['senses'][0]['english_definitions'])
                 ] + [' '.join(Tags)])
             return Card, Kanjis
 
-    furigana, compoundfurigana = CreateFurigana(JishoEntry)
+    furigana = CreateFurigana(JishoEntry)
 
     Card = '\t'.join([
         suffix + JishoEntry['japanese'][0]['word'] + prefix,
         suffix + furigana + prefix,
         ', '.join(JishoEntry['senses'][0]['english_definitions'])
         ] + [' '.join(Tags)])
-
-    if compoundfurigana:
-        # If the borked way of creating furigana was called,
-        # this adds another card, which just puts the reading
-        # as a furigana for the whole word.
-        Card = Card + '\n' + '\t'.join([
-            suffix + JishoEntry['japanese'][0]['word'] + prefix,
-            suffix + compoundfurigana + prefix,
-            ', '.join(JishoEntry['senses'][0]['english_definitions'])
-            ] + [' '.join(Tags)])
 
     return Card, Kanjis
 
@@ -156,7 +150,11 @@ def CreateCards(AllKanjis, LearnedKanjis, n = 1, offset = 0):
     # This will only append new cards if all Kanjis in the
     # new word appear in LearnedKanjis.
     Cards = ''
-    LearnedIndex = AllKanjis.index(LearnedKanjis[-1])
+    try:
+        LearnedIndex = AllKanjis.index(LearnedKanjis[-1])
+    except:
+        LearnedIndex = -1
+
     OffsetKanjis, NewKanjis = AllKanjis[LearnedIndex+1:LearnedIndex+1+offset], AllKanjis[LearnedIndex+1+offset:]
     LearnedKanjis += OffsetKanjis
 
@@ -165,8 +163,12 @@ def CreateCards(AllKanjis, LearnedKanjis, n = 1, offset = 0):
         LearnedKanjis.append(NewKanjis[i])
 
         for word in JishoResponse:
-            Card, Kanjis = CreateCard(word)
-            if all(elem in LearnedKanjis for elem in Kanjis):
+            try:
+                Card, Kanjis = CreateCard(word)
+            except:
+                pass
+
+            if all(elem in ['々'] + LearnedKanjis for elem in Kanjis):
                 Cards = '\n'.join([Cards, Card])
 
     return Cards[1:]
@@ -206,7 +208,7 @@ def main():
                     """)
     LearnedKanjis = [re.sub('\x1f', ' ', LearnedKanji[0]).split()[1] for LearnedKanji in LearnedKanjis]
     LearnedIndex = AllKanjis.index(LearnedKanjis[-1])
-    Cards = CreateCards(AllKanjis, LearnedKanjis, n = 6, offset = 12)
+    Cards = CreateCards(AllKanjis, [], n = 1, offset = 289)
     file = open('newcards', 'w', encoding='utf-8')
     file.write(Cards)
     file.close()
